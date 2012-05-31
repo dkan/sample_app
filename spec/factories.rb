d9339cda88807384ef6aec47e5d75d2a1757613c
Damien Kan <damien.kan@gmail.com>
2012-05-30 21:43:31 -0500

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Damien kan"
-    email    "damien.kan@gmail.com"
-    password "3823epji"
-    password_confirmation "3823epji"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+    
+    factory :admin do
+      admin true
+    end
   end
 end
